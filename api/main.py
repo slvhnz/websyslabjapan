bee84a63f9868cc95a96b53b87d6b3d7540d4c82
itseyans <155883607+itseyans@users.noreply.github.com>
2026-03-14 21:49:02 +0800

    x

--- api/main.py
+++ api/main.py
@@ -52,7 +52,7 @@ app = FastAPI(title="sKonnect API")
 # --- CORS Middleware Setup ---
 origins = [
     "http://localhost:5173",
-    "https://skonnectmalitbog.vercel.app",  # Add your frontend URL here
+    "https://skconnectmalitbog.vercel.app",  # Add your frontend URL here
 ]
                               
 app.add_middleware(
